71f9fff9101ae9fe780d0446555f00118282f2d6
nikk178 <nikita.voxmincev@mail.ru>
2022-03-25 13:14:22 +0300

    Update AutoReload.js

--- AutoReload.js
+++ AutoReload.js
@@ -3,12 +3,11 @@ document.body.addEventListener("mousemove", (event) => {
 const x = event.clientX;
 const y = event.clientY;
 const coor = "Coordinates: (" + x + "," + y + ")";
-
-
 if(reloadTimerId)
-  clearTimeout(reloadTimerId);
-  
+clearTimeout(reloadTimerId);
 reloadTimerId = setTimeout(() => {
+window.scrollTo(0, 0);
 location.reload();
-}, 60000);
-});
+}, 120000);
+
+}); 
